ef36a2ea2ecf56a687c50d888e8dc3aeeaca56c0
juliahoglund <63258050+juliahoglund@users.noreply.github.com>
2023-04-26 22:12:29 +0200

    update sort by chromosome

--- workflow/step_1_extract_ancestor/scripts/sort_by_chromosome.py
+++ workflow/step_1_extract_ancestor/scripts/sort_by_chromosome.py
@@ -13,11 +13,10 @@ This is performed to create one maf file per chromosome,
 since there are multiple maf files per chromosome currently.
 
 :Usage: python sort_by_chromosome.py -p <path to processed files> -f <prefix of the processed files> -s 
-[specied_name] -k [chr1,chr2,chrn...] -o <new path to new files>
+[specied_name] -o <new path to new files>
 
 :Example:
-python sort_by_chromosome.py -p ./data/mRO/ -f mRO_mSTR_mDF_ -s sus_scrofa -k 
-'1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,X,Y,Other' -o sorted
+python sort_by_chromosome.py -p ./data/mRO/ -f mRO_mSTR_mDF_ -s sus_scrofa -o sorted
 
 
 """
@@ -31,6 +30,7 @@ from Bio import Phylo
 from Bio import AlignIO
 from io import StringIO
 from Bio.Align import MultipleSeqAlignment
+from collections import defaultdict
 
 # OptionParser for the input directory (process duplicates output), to be marked ancestor seq, and file identifier.
 parser = OptionParser()
@@ -43,7 +43,6 @@ Where: \
 								<PATH TO PROCESSED FILES> is the path to the files that has been processed in the previous step by mafTools, 'apply_mafTools' \
 		<PREFIX OF THE PROCESSED FILES> is the prefix of the processed files \
 [SPECIES NAME] iis the name of the species, based on which the chromosomes and alignments should be ordered \
-			[CHR1,CHR2,CHRn...] is the chromosomes to be considered, specified one by one \
 									<PATH TO NEW FILES> is the name of the directory that will created in which the new files will be saved")
 
 
@@ -52,7 +51,6 @@ parser.add_option_group(group)
 parser.add_option("-p", "--path", dest="path", help="path to folder with processed files by mafRowOrderer", default= "./mRO")
 parser.add_option("-f", "--fileprefix", dest="prefix", help="prefix of processed files (default = mRO_mStr_mDF_)", default="mRO_mStr_mDF_")
 parser.add_option("-s", "--species", dest="species", help="name of species, based of which the chromosomes should be ordered", default="")
-parser.add_option("-k", "--chromosomes", dest="chromosomes", help="chromosomes that should be considered in the ordering, including possible sex chromosomes and 'Other'", default="1,X,Y")
 parser.add_option("-o", "--ordered", dest="sorted", help="directory within which the new output will be saved", default="sorted")
 # clean intermediate files
 parser.add_option("-c", "--clean", dest="clean", help="remove intermediate files (from previous step) after computation [yes/no]", default="no")
@@ -91,11 +89,8 @@ if bool(file_list) == False:
 	sys.stderr.write("Found no files with the given prefix.\n")
 	sys.exit()
 
-# Create dict for chr and corresponding files
-output_files_chr_dict = {}
-chromosomes = options.chromosomes.split(',')
-for item in chromosomes:
-	output_files_chr_dict[item] = []
+# Create dict for chr and corresponding blocks
+maf_blocks = defaultdict(list)
 
 # Loop through files list and add file names to correct chr
 for maf_f in file_list:
@@ -108,28 +103,41 @@ for maf_f in file_list:
 	else: 
 		open_f = open(options.path + maf_f,'r')
 
-	content_f = open_f.readlines()
 
-	for lines in content_f:
-		if str(options.species) in lines:
-			if "AEMK" in lines:
-				break
-			correct_l = lines
-			ident_chr = correct_l.split()[1].split('.')[1]
-			output_files_chr_dict[ident_chr].append(maf_f)
-			break
+	ident_chr = ""
+	blocks = ""
+	line = open_f.readline() # header
+
+
+	while line != "":
+		line = open_f.readline()
+		if "a " in line:
+			switch = 0
+			blocks = ""
+			blocks += line
+		elif "s " in line:
+			blocks += line
+			if str(options.species) in line:
+				ident_chr = line.split()[1].split('.')[1]
+		else:
+			switch = 1
+		if switch == 1:
+			blocks += '\n'
+			maf_blocks[ident_chr].append(blocks)
+	
+	os.system('gzip ' + str(options.path)+str(maf_f.replace('.gz', '')))
+
 
 # Loop through keys in dict:
-for chr_num, file_names in output_files_chr_dict.items():
-	new_file = open("./"+options.sorted+"/"+options.species+"_chr%s.reordered.maf" %chr_num, "a")
-	new_file.write("##maf version=1\n\n")
-	for fn in file_names:
-		open_inp = open(options.path + fn,'r')
-
-		for line in open_inp.readlines():
+for chr_num, blocks in maf_blocks.items():
+	if "AEMK" in chr_num:
+		continue
+	else:
+		new_file = open("./"+options.sorted+"/"+options.species+"_chr%s.reordered.maf" %chr_num, "a")
+		new_file.write("##maf version=1\n\n")
+		
+		for line in blocks:
 			new_file.write(line)
-
-		os.system('gzip ' + str(options.path)+str(fn))
 
 new_file.close()
 
@@ -140,4 +148,3 @@ if options.clean=='yes':
 indication = open('finished_sort_by_chromosome.txt', 'x')
 indication.close
 os.rename('./finished_sort_by_chromosome.txt', './output/finished_sort_by_chromosome.txt')
-
