dcffaa89d1fdefb45bcab3a79c1c023ee6123495
juliahoglund <63258050+juliahoglund@users.noreply.github.com>
2023-08-01 17:09:19 +0200

    start editing derive variants script

--- workflow/step_4_derive_variants/derive_variants.py
+++ workflow/step_4_derive_variants/derive_variants.py
@@ -7,15 +7,30 @@
 :Date: 06-08.2018
 
 This files goes through the ancestral sequence 
-and compares it with the genome 
+and compares it with the reference genome of interest
 and the known population variants to identfy derived variants.
 
-I make use only of high quality ancestor and assembly
+The identification is based on X criteria:
 Cases		:1		2		3		4		5
 Ancestor	:C		C		C		C		C
 Reference	:C		A>0.9	A		A>0.9	A
 Alternative	:A>0.9	G		G>0.9	C		A
 
+Case:		:1 		  2 		3 			4
+Ancestor 	:C 		  C 		C 			C
+Reference 	:G 		  C 		G 			G
+Population  :ref/alt  C/G>0.9   G/C>0.1 	no data available at position.
+											(likely monomorphic in pop file)
+
+Case 1: The reference genome and ancestor are different at position n
+Case 2: The reference genome and ancestor are the same at position n, 
+		but in the population data, there is a nearly fixed derived variant with >0.9
+Case 3: The reference genome and ancestor are different, but the alternative allele
+		in the population data is not the one in the reference genome, i.e. is most 
+		likely ancestral. Here the population reference allele at >0.9 is what is desired
+Case 4: The reference genome and ancestor are different, but there is no data on that position
+		in the population file. If a true variant and true missingness, it can be assumed to be 100% fixed
+		in the reference species / population, and hence be monomorphic in the reference.
 
 :Edited by: Seyan Hu
 :Date: 19-10-2022
