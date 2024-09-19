9d0a01887bf43d9a59b38f874337c212aafd9b6a
juliahoglund <63258050+juliahoglund@users.noreply.github.com>
2024-09-19 15:48:14 +0200

    Create split2scaffolds.sh

---
+++ GERP/split2scaffolds.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+input=$1
+output=$2
+
+echo splitting $input
+# store variable name
+prefix=`echo $input | sed -r 's/(.*)_sorted.*/\1/' | cut -f3 -d'/'`
+
+# split per scaffold
+csplit -s -z -f $prefix\_ $input '/>/' '{*}'
+echo done splitting $input
+
+# format, rename, fix
+echo changing names..
+for file in $prefix*; do name=`grep '>' $file | tr -d '>'`; mv $file $name:$file; done
+echo formatting sequence labels ..
+for file in *$prefix*; do sed -i "s/^>.*/>$prefix/" $file; done
+echo finishing formatting names and labels ..
+for file in *$prefix*; do name=`echo $file | sed -r 's/(.*)_[[:digit:]]+/\1.fasta/'`; mv $file $name; done
