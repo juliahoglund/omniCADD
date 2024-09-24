30513d88a53a6eb4394bedc96e93f1e3aea0f46c
juliahoglund <63258050+juliahoglund@users.noreply.github.com>
2024-09-24 10:13:51 +0200

    add script dir option, update system commands to search for scripts based on path input

--- workflow/scripts/step_5_annotate_variants/sift/make-SIFT-db-all.pl
+++ workflow/scripts/step_5_annotate_variants/sift/make-SIFT-db-all.pl
@@ -1,8 +1,15 @@
 #!/usr/bin/perl -w
 
 # This script's original distribution:
+# https://github.com/pauline-ng/SIFT4G_Create_Genomic_DB/blob/master/make-SIFT-db-all.pl
+# Modifications: Julia Höglund
+# Date: 2024-09-23
+# Changes:
+# - add script directory to getoptions
+# - add redirection to scripts when lookin for metadocs
 
 # create the SIFT databases, must be run in scripts directory because it's lookin for metadocs
+
 
 use strict;
 #require 'common-utils.pl'; 
@@ -20,15 +27,18 @@ my %options  = ();
 my $help = "";
 my $ensembl_download = "";
 my $metafile = "";
+my $script_dir = "";
 GetOptions ('help|h' => \$help, 
 	    'ensembl_download' => \$ensembl_download,
-	    'config=s' => \$metafile );
+	    'config=s' => \$metafile,
+	    'script_directory=s' => \$script_dir);
 
-if ($help || !$metafile || !$metafile) {
+if ($help || !$metafile || !$metafile || !$script_dir) {
 	print "make-SIFT-db-all.pl\n";
 	print " -h  help\n";
 	print " -config  [required config file]\n";
 	print " -ensembl_download \n";
+	print " -script_directory  [required path to sift scripts]\n"
 }
 
 my $meta_href = readMeta($metafile);
@@ -49,27 +59,37 @@ for my $key (keys %meta_hash) {
 my $siftscore_dir = $meta_hash{"PARENT_DIR"} . "/" . $meta_hash{"SINGLE_REC_WITH_SIFTSCORE_DIR"};
 if (! -d $siftscore_dir) { mkdir ($siftscore_dir, 0775); } 		
 
+
+# changed from orinal; 
+# store paths so this script does not need to be run in the script directory
+my $ensembl_annotation = $script_dir . '/download-annotation-srcs.pl';
+my $ensembl_fasta = $script_dir . '/download-fasta.pl';
+my $ensembl_dbSNP = $script_dir . '/download-dbSNP-files.pl';
+my $split_dbSNP = $script_dir . '/split-dbSNP-by-chr.pl';
+
 if ($ensembl_download) {
 	# not manual, this is from Ensembl and needs to download
 	print "downloading gene annotation\n";
-	`perl download-annotation-srcs.pl $metafile`; 
+	`perl $ensembl_annotation $metafile`; 
 	print "done downloading gene annotation\n"; 
 
 	print "downloading fasta files\n"; 
-	`perl download-fasta.pl $metafile`; 
+	`perl $ensembl_fasta $metafile`; 
 	print "done downloading DNA fasta sequences"; 
 
 	print "download dbSNP files\n";
-	`perl download-dbSNP-files.pl $metafile`; 
+	`perl $ensembl_dbSNP $metafile`; 
 	if (exists ($meta_hash{"DBSNP_VCF_FILE"}) && $meta_hash{"DBSNP_VCF_FILE"} ne "") 
 	{
 		print "splitting dbSNP file\n";
-		`perl split-dbSNP-by-chr.pl $metafile`;
+		`perl $split_dbSNP $metafile`;
 	}
 } # end if downloading files from Ensembl
 
+my $convert_ucsc = $script_dir . '/gff_gene_format_to_ucsc.pl';
+
 print "converting gene format to use-able input\n";
-`perl gff_gene_format_to_ucsc.pl $metafile`; 
+`perl $convert_ucsc $metafile`; 
 #`perl ensembl_gene_format_to_ucsc.pl $metafile`;
 print "done converting gene format\n";
 
@@ -89,16 +109,20 @@ if (glob ("$fasta_dir/*.gz")) {
         die "no DNA fasta files in $fasta_dir\n";
 }
 
+my $records = $script_dir . '/make-single-records-BIOPERL.pl';
+my $noncoding = $script_dir . '/make-single-records-noncoding.pl';
+my $fasta_seqs = $script_dir . '/generate-fasta-subst-files-BIOPERL.pl';
+
 print "making single records file\n"; 
-`perl make-single-records-BIOPERL.pl $metafile`; 
+`perl $records $metafile`; 
 print "done making single records template\n"; 
 
 print "making noncoding records file\n";
-`perl make-single-records-noncoding.pl $metafile`;
+`perl $noncoding $metafile`;
 print "done making noncoding records\n";
 
 print "make the fasta sequences\n"; 
-`perl generate-fasta-subst-files-BIOPERL.pl $metafile`; 
+`perl $fasta_seqs $metafile`; 
 print "done making the fasta sequences\n";
 
 print "start siftsharp, getting the alignments\n"; 
@@ -130,23 +154,28 @@ print "done getting all the scores\n";
 print "populating databases\n";
 # check quality
 # Pauline to do , by chromosame in script
+my $populate_db = $script_dir . '/make-sift-scores-db-batch.pl';
+my $check_genes = $script_dir . '/check_genes.pl'; 
 
-`perl make-sift-scores-db-batch.pl $metafile`;
+`perl $populate_db $metafile`;
 
 print "checking the databases\n";
-system ("perl check_genes.pl $metafile");
+system ("perl $check_genes $metafile");
 
 my @chromosomes =  getChr ($meta_hash{"PARENT_DIR"} . "/" . $meta_hash{"GENE_DOWNLOAD_DEST"});
+my $check_siftdb = $script_dir . '/check_SIFTDB.py';
+my $check_pep = $script_dir . '/checkENSPep.pl';
+
 
 foreach my $chr (@chromosomes) {
 
         my $db_file = $meta_hash{"PARENT_DIR"} . "/" . $meta_hash{"SINGLE_REC_WITH_SIFTSCORE_DIR"} . "/" . $chr .  "_scores.Srecords.with_dbSNPid.sorted";
         my $check_outfile = $meta_hash{"PARENT_DIR"}  .  "/" . $meta_hash{"ORG_VERSION"} . "/" . $chr .  "_SIFTDB_stats.txt";
-        `python check_SIFTDB.py $db_file $check_outfile`;
+        `python $check_siftdb $db_file $check_outfile`;
 }
 
 my $pep_check_file = $meta_hash{"PARENT_DIR"} . "/ENS_peptide_check.txt";
-`perl checkENSPep.pl $metafile ENS $pep_check_file`; 
+`perl $check_pep $metafile ENS $pep_check_file`; 
 
 my $final_outfolder = $meta_hash{"PARENT_DIR"} ."/". $meta_hash{"ORG_VERSION"};
 system ("cp  $metafile $final_outfolder"); 
