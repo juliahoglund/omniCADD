b6e62f1f433cc63cc2416e5df8340d20eb4c3182
juliahoglund <63258050+juliahoglund@users.noreply.github.com>
2025-01-07 10:07:37 +0100

    Update wig2bed.R

--- workflow/scripts/step_5_annotate_variants/wig2bed.R
+++ workflow/scripts/step_5_annotate_variants/wig2bed.R
@@ -14,7 +14,7 @@ library(optparse)
 rm(list=ls())
 
 option_list = list(
-  make_option(c("-d", "--directory"), type="character", default="results/annotation/phast/phastCons/chr1",
+  make_option(c("-d", "--directory"), type="character", default="results/annotation/phast/phastCons/chr1/",
               help="directory with phastCons / phyloP wig scores", metavar="character"),
   make_option(c("-t", "--type"), type="character", default = "phast",
               help="type of analysis [phast | phylo", metavar="character")
@@ -27,7 +27,7 @@ files <- naturalsort(list.files(path = opt$directory, pattern = ".wig"))
 
 for (i in 1:length(files)) {
   chromosome <- unlist(str_split(unlist(str_split(files[i], pattern = '_'))[1], pattern = 'chr'))[2]
-  part <- fread(files[i])
+  part <- fread(paste(opt$directory, files[i], sep = ""))
   part <- as.data.table(part)
   part$chromosome <- chromosome
   if (i == 1) {   
