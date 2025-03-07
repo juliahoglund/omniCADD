965392df2193ecfd8d9adc455647bc0122f5cb8a
juliahoglund <63258050+juliahoglund@users.noreply.github.com>
2025-03-07 15:52:22 +0100

    add information and logging

--- workflow/scripts/step_5_annotate_variants/split_alignments.py
+++ workflow/scripts/step_5_annotate_variants/split_alignments.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+Split a multi-FASTA alignment file into smaller chunks.
+Derived from: split_fasta.py (from: https://thevirtuallaboratory.com/blog/splitting-a-multi-fasta)
+Author: Bram van Dijk
+Modified by: Julia Höglund 2025-03-05
+"""
 
-
-# derived from:
-# split_fasta.py (assumes you have biopython installed, e.g. with pip install biopython)
-# from: https://thevirtuallaboratory.com/blog/splitting-a-multi-fasta
-# Author: Bram van Dijk
-
-# dependencies
-import sys, math
+import sys
+import math
 import gzip
 import Bio
 from Bio import AlignIO
+import os
 
 def batch_iterator(iterator, batch_size):
     """
     This is a generator function, and it returns lists of the
-    entries from the supplied iterator.  Each list will have
+    entries from the supplied iterator. Each list will have
     batch_size entries, although the final list may be shorter.
     (derived from https://biopython.org/wiki/Split_large_file)
     """
@@ -30,23 +31,24 @@ def batch_iterator(iterator, batch_size):
             except StopIteration:
                 entry = None
             if entry is None:
-                break # EOF = end of file
+                break  # EOF = end of file
             batch.append(entry)
         if batch:
             yield batch
 
-
-if(len(sys.argv) != 5):
-        sys.exit("usage: convert_alignments.py MAF_FILE N_CHUNKS OUTPUT_FOLDER_MAF REF_SPECIES")
+if len(sys.argv) != 5:
+    sys.exit("usage: convert_alignments.py MAF_FILE N_CHUNKS OUTPUT_FOLDER_MAF REF_SPECIES")
 
 mfile = sys.argv[1]  # maf file
-ofile = gzip.open(mfile, "rt") \
-                if mfile.endswith('.gz') else open(mfile, "r") # maf file
+ofile = gzip.open(mfile, "rt") if mfile.endswith('.gz') else open(mfile, "r")  # maf file
 
+chunks = sys.argv[2]  # number of chunks
+maf_folder = sys.argv[3]  # folder to save maf chunks in
+ref_species = sys.argv[4]
 
-chunks=sys.argv[2] # number of chunks
-maf_folder=sys.argv[3] # folder to save maf chunks in
-ref_species=sys.argv[4]
+if not os.path.exists(maf_folder):
+    os.makedirs(maf_folder)
+
 to_keep = []
 
 # parse alignment
@@ -55,13 +57,12 @@ for alignment in AlignIO.parse(ofile, "maf"):
         to_keep.append(alignment)
 
 nseq = len(to_keep)
-chunksize=math.ceil(nseq/int(chunks))
+chunksize = math.ceil(nseq / int(chunks))
 chrom = mfile.split('.')[0].split('chr')[1]
 
 print("Splitting maffile file of", nseq, "blocks into chunks of", chunksize, "blocks")
 for i, batch in enumerate(batch_iterator(to_keep, chunksize)):
-
-    filename = str(maf_folder) + "chr" + str(chrom) + "-%i.maf" % (i + 1) # with or w/o
+    filename = os.path.join(maf_folder, f"chr{chrom}-{i + 1}.maf")
     with open(filename, "w") as maf_handle:
         count = Bio.AlignIO.write(batch, maf_handle, "maf")
     print("Wrote %i sequences to %s" % (count, filename))
