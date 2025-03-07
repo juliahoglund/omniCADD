1ad349b20e7d6ac56aa245b882dc936a3d37423b
juliahoglund <63258050+juliahoglund@users.noreply.github.com>
2025-03-07 15:50:33 +0100

    wrap in functions, add logging and debug

--- workflow/scripts/step_5_annotate_variants/maf2fasta.pl
+++ workflow/scripts/step_5_annotate_variants/maf2fasta.pl
@@ -5,73 +5,76 @@
 # Convert MAF to FASTA 
 # Optionally only convert blocks that contain label 
 # ./maf2fasta.pl [label] < maf > fasta
+# Modified by: Julia Höglund 2025-03-05
 
 use strict; 
+use warnings;  # Add warnings for better debugging
+
+# Initialize variables
 my $currscore; 
 my $currlabel; 
 my $currcoord; 
 my $currorient; 
-my $saveblock=0; 
+my $saveblock = 0; 
 my @matches; 
 my @blocks; 
 
-while(my $line=<STDIN>){
-
-    if($line =~ /a\s+score=([\d\.\-]+)/){
-	if($saveblock>0){
-	    my @nmatches = @matches;
-	    push @blocks,[$currscore,$currlabel,$currorient,$currcoord,\@nmatches];
-	}
-	($currscore) = ($line =~ /a\s+score=([\d\.\-]+)/);
-	($currlabel) = ($line =~ /label=(\d+)/);
-	@matches=();
+# Read input line by line
+while (my $line = <STDIN>) {
+    if ($line =~ /a\s+score=([\d\.\-]+)/) {
+        if ($saveblock > 0) {
+            my @nmatches = @matches;
+            push @blocks, [$currscore, $currlabel, $currorient, $currcoord, \@nmatches];
+        }
+        ($currscore) = ($line =~ /a\s+score=([\d\.\-]+)/);
+        ($currlabel) = ($line =~ /label=(\d+)/);
+        @matches = ();
     }
-    elsif($line =~ /s\s+(\S+)\s+(\d+)\s+(\d+)\s+([+-])\s+(\d+)\s+(\S+)/){
-	my $accession = $1;
-	my $start = $2;
-	my $len = $3;
-	my $orientation = $4;
-	my $seqlength = $5;
-	my $seq = $6;
-	if($accession =~ /([^\.]+)\.(\S+)/){
-	    
-	}
-	else{
-	    die if($accession =~ /\./);
-	    #$accession = "$accession.$accession";
-	}
-	push @matches,[$accession,$start,$len,$orientation,$seqlength,$seq];
-	$saveblock=1;
-    }
-    else{
-	
+    elsif ($line =~ /s\s+(\S+)\s+(\d+)\s+(\d+)\s+([+-])\s+(\d+)\s+(\S+)/) {
+        my $accession = $1;
+        my $start = $2;
+        my $len = $3;
+        my $orientation = $4;
+        my $seqlength = $5;
+        my $seq = $6;
+        if ($accession =~ /([^\.]+)\.(\S+)/) {
+            # Add code to handle this case if needed
+        }
+        else {
+            die "Invalid accession format: $accession" if ($accession =~ /\./);
+        }
+        push @matches, [$accession, $start, $len, $orientation, $seqlength, $seq];
+        $saveblock = 1;
     }
 }
 
-if($saveblock>0){
+# Save the last block if needed
+if ($saveblock > 0) {
     my @nmatches = @matches;
-    push @blocks,[$currscore,$currlabel,$currorient,$currcoord,\@nmatches];
+    push @blocks, [$currscore, $currlabel, $currorient, $currcoord, \@nmatches];
 }
 
-foreach my $block (sort {$a->[3] <=> $b->[3]} @blocks){
-    if($ARGV[0]){
-	if($block->[1] eq $ARGV[0]){
-	    &printFASTA(@$block) ;
-	}
+# Sort and print blocks
+foreach my $block (sort { $a->[3] <=> $b->[3] } @blocks) {
+    if ($ARGV[0]) {
+        if ($block->[1] && $block->[1] eq $ARGV[0]) {  # Ensure $block->[1] is defined
+            printFASTA(@$block);
+        }
     }
-    else{
-	&printFASTA(@$block) ;
+    else {
+        printFASTA(@$block);
     }
 }
 
-sub printFASTA{
-    my($score,$label,$orient,$coord,$matches) = @_;
-    foreach my $m (@$matches){
-	#print ">$m->[0].$label score=$score $m->[1] $m->[2] $m->[3] $m->[4]\n";
-	print ">$m->[0] $m->[1] $m->[2] $m->[3] $m->[4]\n";
-	for(my $i=0;$i<length($m->[5]);$i+=60){
-	    print substr($m->[5],$i,60),"\n";
-	}
+# Subroutine to print FASTA format
+sub printFASTA {
+    my ($score, $label, $orient, $coord, $matches) = @_;
+    foreach my $m (@$matches) {
+        # print ">$m->[0].$label score=$score $m->[1] $m->[2] $m->[3] $m->[4]\n";
+        print ">$m->[0] $m->[1] $m->[2] $m->[3] $m->[4]\n";
+        for (my $i = 0; $i < length($m->[5]); $i += 60) {
+            print substr($m->[5], $i, 60), "\n";
+        }
     }
     print "=\n";
 }
