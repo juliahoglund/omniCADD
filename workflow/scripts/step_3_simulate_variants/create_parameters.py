8c4bbc4237f005311772ca6afbdbc40e5ba855ef
juliahoglund <63258050+juliahoglund@users.noreply.github.com>
2025-03-07 15:35:40 +0100

    wrap script in function

--- workflow/scripts/step_3_simulate_variants/create_parameters.py
+++ workflow/scripts/step_3_simulate_variants/create_parameters.py
@@ -52,6 +52,11 @@ import sys
 from argparse import ArgumentParser
 from collections import defaultdict
 
+# Constants for nucleotide characters
+NUCLEOTIDES = ["A", "C", "G", "T"]
+GAP = "-"
+UNKNOWN = "N"
+
 parser = ArgumentParser(description=__doc__)
 parser.add_argument("-a", "--ancestor",
                     help="extracted ancestor sequence",
@@ -86,255 +91,264 @@ def get_nucleotides_list(file) -> list:
     return list_nt
 
 
-list_anc = get_nucleotides_list(args.ancestor)
-list_ref = get_nucleotides_list(args.reference)
+def main():
+    try:
+        list_anc = get_nucleotides_list(args.ancestor)
+        list_ref = get_nucleotides_list(args.reference)
 
-# Total counts and mutation counts.
-total = 0
-mut = 0
-totalCpG = 0
-mutCpG = 0
-# Total nt in reference.
-totalrefA = 0
-totalrefC = 0
-totalrefG = 0
-totalrefT = 0
-# Total nt substitution counts.
-ACn = 0
-AGn = 0
-ATn = 0
-CAn = 0
-CGn = 0
-CTn = 0
-GAn = 0
-GCn = 0
-GTn = 0
-TAn = 0
-TCn = 0
-TGn = 0
-# Total nt substitution counts in CpG sites.
-CA = 0
-CG = 0
-CT = 0
-GA = 0
-GC = 0
-GT = 0
-# Insertions and deletions counts.
-nt_insertion = 0
-nt_deletion = 0
-gap = 0
-dict_insertion = {}
-dict_deletion = {}
+        # Total counts and mutation counts.
+        total = 0
+        mut = 0
+        totalCpG = 0
+        mutCpG = 0
+        # Total nt in reference.
+        totalrefA = 0
+        totalrefC = 0
+        totalrefG = 0
+        totalrefT = 0
+        # Total nt substitution counts.
+        ACn = 0
+        AGn = 0
+        ATn = 0
+        CAn = 0
+        CGn = 0
+        CTn = 0
+        GAn = 0
+        GCn = 0
+        GTn = 0
+        TAn = 0
+        TCn = 0
+        TGn = 0
+        # Total nt substitution counts in CpG sites.
+        CA = 0
+        CG = 0
+        CT = 0
+        GA = 0
+        GC = 0
+        GT = 0
+        # Insertions and deletions counts.
+        nt_insertion = 0
+        nt_deletion = 0
+        gap = 0
+        dict_insertion = {}
+        dict_deletion = {}
 
-# Calc total nt.
-len_seq = len(list_anc)
-total += len_seq
+        # Calc total nt.
+        len_seq = len(list_anc)
+        total += len_seq
 
-print('Start counting! Chr: ' + args.chr)
+        print('Start counting! Chr: ' + args.chr)
 
-# Iterate through nt and compare.
-for num, nt_ref in enumerate(list_ref):
-    nt_anc = list_anc[num].upper()
+        # Iterate through nt and compare.
+        for num, nt_ref in enumerate(list_ref):
+            nt_anc = list_anc[num].upper()
 
-    # Checks the nt from the reference counts.
-    if nt_ref == 'G':
-        totalrefG += 1
-    elif nt_ref == 'C':
-        totalrefC += 1
-    elif nt_ref == 'A':
-        totalrefA += 1
-    elif nt_ref == 'T':
-        totalrefT += 1
+            # Checks the nt from the reference counts.
+            if nt_ref == 'G':
+                totalrefG += 1
+            elif nt_ref == 'C':
+                totalrefC += 1
+            elif nt_ref == 'A':
+                totalrefA += 1
+            elif nt_ref == 'T':
+                totalrefT += 1
 
-    # Check for insertion, gaps, deletion counts.
-    # Append the 2 nt (ancestor-reference).
-    list_nt_anc_ref = nt_anc.upper() + nt_ref.upper()
-    # Check for insertions (when the nt is '-' (a gap) in ancestor but in
-    # ref a C, G, A or T).
-    if list_nt_anc_ref.startswith('-') and not list_nt_anc_ref.endswith('N'):
-        nt_insertion += 1
-    # Check for gaps in both sequences.
-    elif list_nt_anc_ref.startswith('-') and list_nt_anc_ref.endswith('N'):
-        gap += 1
-    # Checks for deletions (when the nt is not '-' (so a C, G, A or T) in
-    # ancestor but in ref a N).
-    elif not list_nt_anc_ref.startswith('-') and list_nt_anc_ref.endswith('N'):
-        nt_deletion += 1
+            # Check for insertion, gaps, deletion counts.
+            # Append the 2 nt (ancestor-reference).
+            list_nt_anc_ref = nt_anc.upper() + nt_ref.upper()
+            # Check for insertions (when the nt is '-' (a gap) in ancestor but in
+            # ref a C, G, A or T).
+            if list_nt_anc_ref.startswith(GAP) and not list_nt_anc_ref.endswith(UNKNOWN):
+                nt_insertion += 1
+            # Check for gaps in both sequences.
+            elif list_nt_anc_ref.startswith(GAP) and list_nt_anc_ref.endswith(UNKNOWN):
+                gap += 1
+            # Checks for deletions (when the nt is not '-' (so a C, G, A or T) in
+            # ancestor but in ref a N).
+            elif not list_nt_anc_ref.startswith(GAP) and list_nt_anc_ref.endswith(UNKNOWN):
+                nt_deletion += 1
 
-    # Check for substitution counts.
-    # Checks if ancestor nt starts with 'A'.
-    elif list_nt_anc_ref.startswith('A'):
-        if list_nt_anc_ref.endswith('C'):
-            ACn += 1
-            mut += 1
-        elif list_nt_anc_ref.endswith('G'):
-            AGn += 1
-            mut += 1
-        elif list_nt_anc_ref.endswith('T'):
-            ATn += 1
-            mut += 1
-    # Checks if ancestor nt starts with 'T'.
-    elif list_nt_anc_ref.startswith('T'):
-        if list_nt_anc_ref.endswith('C'):
-            TCn += 1
-            mut += 1
-        elif list_nt_anc_ref.endswith('G'):
-            TGn += 1
-            mut += 1
-        elif list_nt_anc_ref.endswith('A'):
-            TAn += 1
-            mut += 1
+            # Check for substitution counts.
+            # Checks if ancestor nt starts with 'A'.
+            elif list_nt_anc_ref.startswith('A'):
+                if list_nt_anc_ref.endswith('C'):
+                    ACn += 1
+                    mut += 1
+                elif list_nt_anc_ref.endswith('G'):
+                    AGn += 1
+                    mut += 1
+                elif list_nt_anc_ref.endswith('T'):
+                    ATn += 1
+                    mut += 1
+            # Checks if ancestor nt starts with 'T'.
+            elif list_nt_anc_ref.startswith('T'):
+                if list_nt_anc_ref.endswith('C'):
+                    TCn += 1
+                    mut += 1
+                elif list_nt_anc_ref.endswith('G'):
+                    TGn += 1
+                    mut += 1
+                elif list_nt_anc_ref.endswith('A'):
+                    TAn += 1
+                    mut += 1
 
-    # Check for CpG/GpC sites followed by counting and for substitution
-    # counts for C and G to another nt. Check if ancestor nt starts with 'C'.
-    elif list_nt_anc_ref.startswith('C'):
-        # Checks if it is a CpG/GpC site.
-        # Does not count as normal substitution.
-        if (num > 0 and list_anc[num - 1] == 'G') or (
-                num < (len_seq - 1) and list_anc[num + 1] == 'G'):
-            # Counts CpG/GpC sites occurrences.
-            totalCpG += 1
-            # Checks for mutations in the site.
-            if list_nt_anc_ref.endswith('A'):
-                CA += 1
-                mutCpG += 1
-            elif list_nt_anc_ref.endswith('G'):
-                CG += 1
-                mutCpG += 1
-            elif list_nt_anc_ref.endswith('T'):
-                CT += 1
-                mutCpG += 1
-        # If not in site, mutations are still checked.
-        else:
-            if list_nt_anc_ref.endswith('A'):
-                CAn += 1
-                mut += 1
-            elif list_nt_anc_ref.endswith('G'):
-                CGn += 1
-                mut += 1
-            elif list_nt_anc_ref.endswith('T'):
-                CTn += 1
-                mut += 1
-    # Check if ancestor nt starts with 'G'.
-    elif list_nt_anc_ref.startswith('G'):
-        # Checks if it is a CpG/GpC site.
-        if (num > 0 and list_anc[num - 1] == 'C') or (
-                num < (len_seq - 1) and list_anc[num + 1] == 'C'):
-            # Counts CpG/GpC sites occurences.				totalCpG += 1
-            # Checks for mutations in the site.
-            if list_nt_anc_ref.endswith('A'):
-                GA += 1
-                mutCpG += 1
-            elif list_nt_anc_ref.endswith('C'):
-                GC += 1
-                mutCpG += 1
-            elif list_nt_anc_ref.endswith('T'):
-                GT += 1
-                mutCpG += 1
-        # If not in site, mutations are still checked.
-        else:
-            if list_nt_anc_ref.endswith('A'):
-                GAn += 1
-                mut += 1
-            elif list_nt_anc_ref.endswith('C'):
-                GCn += 1
-                mut += 1
-            elif list_nt_anc_ref.endswith('T'):
-                GTn += 1
-                mut += 1
+            # Check for CpG/GpC sites followed by counting and for substitution
+            # counts for C and G to another nt. Check if ancestor nt starts with 'C'.
+            elif list_nt_anc_ref.startswith('C'):
+                # Checks if it is a CpG/GpC site.
+                # Does not count as normal substitution.
+                if (num > 0 and list_anc[num - 1] == 'G') or (
+                        num < (len_seq - 1) and list_anc[num + 1] == 'G'):
+                    # Counts CpG/GpC sites occurrences.
+                    totalCpG += 1
+                    # Checks for mutations in the site.
+                    if list_nt_anc_ref.endswith('A'):
+                        CA += 1
+                        mutCpG += 1
+                    elif list_nt_anc_ref.endswith('G'):
+                        CG += 1
+                        mutCpG += 1
+                    elif list_nt_anc_ref.endswith('T'):
+                        CT += 1
+                        mutCpG += 1
+                # If not in site, mutations are still checked.
+                else:
+                    if list_nt_anc_ref.endswith('A'):
+                        CAn += 1
+                        mut += 1
+                    elif list_nt_anc_ref.endswith('G'):
+                        CGn += 1
+                        mut += 1
+                    elif list_nt_anc_ref.endswith('T'):
+                        CTn += 1
+                        mut += 1
+            # Check if ancestor nt starts with 'G'.
+            elif list_nt_anc_ref.startswith('G'):
+                # Checks if it is a CpG/GpC site.
+                if (num > 0 and list_anc[num - 1] == 'C') or (
+                        num < (len_seq - 1) and list_anc[num + 1] == 'C'):
+                    # Counts CpG/GpC sites occurrences.
+                    totalCpG += 1
+                    # Checks for mutations in the site.
+                    if list_nt_anc_ref.endswith('A'):
+                        GA += 1
+                        mutCpG += 1
+                    elif list_nt_anc_ref.endswith('C'):
+                        GC += 1
+                        mutCpG += 1
+                    elif list_nt_anc_ref.endswith('T'):
+                        GT += 1
+                        mutCpG += 1
+                # If not in site, mutations are still checked.
+                else:
+                    if list_nt_anc_ref.endswith('A'):
+                        GAn += 1
+                        mut += 1
+                    elif list_nt_anc_ref.endswith('C'):
+                        GCn += 1
+                        mut += 1
+                    elif list_nt_anc_ref.endswith('T'):
+                        GTn += 1
+                        mut += 1
 
-# Checks for insertions and deletions.
-# Dictionaries for insertions and deletions (length(key), count(value)).
-insertionsizes = defaultdict(int)
-insertion = 0
-deletionsizes = defaultdict(int)
-deletion = 0
+        # Checks for insertions and deletions.
+        # Dictionaries for insertions and deletions (length(key), count(value)).
+        insertionsizes = defaultdict(int)
+        insertion = 0
+        deletionsizes = defaultdict(int)
+        deletion = 0
 
-print("Measuring indels ...")
-# Iterate over the nt in the sequence.
-position = 0
-for position in range(1, len_seq + 1):
-    nt_anc = list_anc[position - 1].upper()
-    nt_ref = list_ref[position - 1].upper()
+        print("Measuring indels ...")
+        # Iterate over the nt in the sequence.
+        position = 0
+        for position in range(1, len_seq + 1):
+            nt_anc = list_anc[position - 1].upper()
+            nt_ref = list_ref[position - 1].upper()
 
-    # If there is an nt insertion found add 1 to insertion (size), if it is
-    # the start of insertion or previous nt was also an insertion.
-    if nt_anc == "-" and ((insertion > 0) or (
-            insertion == 0 and nt_ref in ["A", "C", "G", "T"])):
-        insertion += 1
-    # Else (at end of insertion) add the insertion size to the dict and add
-    # a count to it. Afterwards set the size ('insertion') back to 0.
-    elif nt_ref in ["A", "C", "G", "T"]:
-        if insertion > 0:
-            insertionsizes[insertion] += 1
-            insertion = 0
+            # If there is an nt insertion found add 1 to insertion (size), if it is
+            # the start of insertion or previous nt was also an insertion.
+            if nt_anc == GAP and ((insertion > 0) or (
+                    insertion == 0 and nt_ref in NUCLEOTIDES)):
+                insertion += 1
+            # Else (at end of insertion) add the insertion size to the dict and add
+            # a count to it. Afterwards set the size ('insertion') back to 0.
+            elif nt_ref in NUCLEOTIDES:
+                if insertion > 0:
+                    insertionsizes[insertion] += 1
+                    insertion = 0
 
-    # If there is an nt deletion found add 1 to deletion (size), if it is
-    # the start of deletion or previous nt was also a deletion.
-    if nt_ref == "N" and ((deletion > 0) or (
-            deletion == 0 and nt_anc in ["A", "C", "G", "T"])):
-        deletion += 1
-    # At the end of the deletion add the size to the dict and add a count to
-    # it. Afterwards set the size back to 0.
-    elif nt_anc in ["A", "C", "G", "T"]:
-        if deletion > 0:
-            deletionsizes[deletion] += 1
-            deletion = 0
+            # If there is an nt deletion found add 1 to deletion (size), if it is
+            # the start of deletion or previous nt was also a deletion.
+            if nt_ref == UNKNOWN and ((deletion > 0) or (
+                    deletion == 0 and nt_anc in NUCLEOTIDES)):
+                deletion += 1
+            # At the end of the deletion add the size to the dict and add a count to
+            # it. Afterwards set the size back to 0.
+            elif nt_anc in NUCLEOTIDES:
+                if deletion > 0:
+                    deletionsizes[deletion] += 1
+                    deletion = 0
 
-# Define controls.
-chrom, window_start, pos = args.chr, 1, position
-cmut = mut
-ctotal = total
-cmutCpG = mutCpG
-ctotalCpG = totalCpG
-cA, cC, cG, cT = totalrefA, totalrefC, totalrefG, totalrefT
+        # Define controls.
+        chrom, window_start, pos = args.chr, 1, position
+        cmut = mut
+        ctotal = total
+        cmutCpG = mutCpG
+        ctotalCpG = totalCpG
+        cA, cC, cG, cT = totalrefA, totalrefC, totalrefG, totalrefT
 
-# Create output.
-'''
-:Output format:
-###CHROM <num>
-chrom	window_start	pos	cmut	ctotal	cmutCpG	ctotalCpG	cA	cC	cG	cT
-##STATS
-#A	C	G	T	CpGs
-...
-#y	N	AC	AG	AT	CA	CG	CT	GA	GC	GT	TA	TC	TG
-...
-#yCpG	NCpG	CA	CG	CT	GA	GC	GT
-...
-##INSERTIONS
-#len	count
-...
-##DELETIONS
-#len	count
-...
-'''
+        # Create output.
+        '''
+        :Output format:
+        ###CHROM <num>
+        chrom	window_start	pos	cmut	ctotal	cmutCpG	ctotalCpG	cA	cC	cG	cT
+        ##STATS
+        #A	C	G	T	CpGs
+        ...
+        #y	N	AC	AG	AT	CA	CG	CT	GA	GC	GT	TA	TC	TG
+        ...
+        #yCpG	NCpG	CA	CG	CT	GA	GC	GT
+        ...
+        ##INSERTIONS
+        #len	count
+        ...
+        ##DELETIONS
+        #len	count
+        ...
+        '''
 
-print("Writing output.")
-output = open(args.outfile, 'w')
+        print("Writing output.")
+        with open(args.outfile, 'w') as output:
+            # Write to output.
+            output.write("###CHROM " + str(args.chr) + "\n")
+            output.write("%s\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n" % (
+                chrom, window_start, pos, cmut, ctotal, cmutCpG, ctotalCpG, cA, cC, cG, cT))
 
-# Write to output.
-output.write("###CHROM " + str(args.chr) + "\n")
-output.write("%s\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n" % (
-    chrom, window_start, pos, cmut, ctotal, cmutCpG, ctotalCpG, cA, cC, cG, cT))
+            output.write("\n##STATS\n")
+            output.write("#A\tC\tG\tT\tCpGs\n")
+            output.write("\t".join(
+                map(str, [totalrefA, totalrefC, totalrefG, totalrefT, totalCpG])) + "\n")
+            output.write("#y\tN\tAC\tAG\tAT\tCA\tCG\tCT\tGA\tGC\tGT\tTA\tTC\tTG\n")
+            output.write("\t".join(map(str,
+                                       [mut, total, ACn, AGn, ATn, CAn, CGn, CTn, GAn, GCn,
+                                        GTn, TAn, TCn, TGn])) + "\n")
+            output.write("#yCpG\tNCpG\tCA\tCG\tCT\tGA\tGC\tGT\n")
+            output.write(
+                "\t".join(map(str, [mutCpG, totalCpG, CA, CG, CT, GA, GC, GT])) + "\n")
 
-output.write("\n##STATS\n")
-output.write("#A\tC\tG\tT\tCpGs\n")
-output.write("\t".join(
-    map(str, [totalrefA, totalrefC, totalrefG, totalrefT, totalCpG])) + "\n")
-output.write("#y\tN\tAC\tAG\tAT\tCA\tCG\tCT\tGA\tGC\tGT\tTA\tTC\tTG\n")
-output.write("\t".join(map(str,
-                           [mut, total, ACn, AGn, ATn, CAn, CGn, CTn, GAn, GCn,
-                            GTn, TAn, TCn, TGn])) + "\n")
-output.write("#yCpG\tNCpG\tCA\tCG\tCT\tGA\tGC\tGT\n")
-output.write(
-    "\t".join(map(str, [mutCpG, totalCpG, CA, CG, CT, GA, GC, GT])) + "\n")
+            output.write("##INSERTIONS\n")
+            output.write("#len\tcount\n")
+            for key, value in insertionsizes.items():
+                output.write("%d\t%d\n" % (key, value))
 
-output.write("##INSERTIONS\n")
-output.write("#len\tcount\n")
-for key, value in insertionsizes.items():
-    output.write("%d\t%d\n" % (key, value))
+            output.write("##DELETIONS\n")
+            output.write("#len\tcount\n")
+            for key, value in deletionsizes.items():
+                output.write("%d\t%d\n" % (key, value))
 
-output.write("##DELETIONS\n")
-output.write("#len\tcount\n")
-for key, value in deletionsizes.items():
-    output.write("%d\t%d\n" % (key, value))
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
