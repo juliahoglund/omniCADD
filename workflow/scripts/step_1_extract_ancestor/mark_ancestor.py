0dfb742c886da7c4f7eb8a6856ecb6280c719745
juliahoglund <63258050+juliahoglund@users.noreply.github.com>
2025-03-07 15:24:15 +0100

    fix typos, clean script

--- workflow/scripts/step_1_extract_ancestor/mark_ancestor.py
+++ workflow/scripts/step_1_extract_ancestor/mark_ancestor.py
@@ -38,34 +38,39 @@ from argparse import ArgumentParser
 import gzip
 from Bio import Phylo
 from io import StringIO
-from sh import gunzip
+import logging
+import sys
+import os
 
-parser = ArgumentParser(description = __doc__)
+# Set up logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+parser = ArgumentParser(description=__doc__)
 parser.add_argument('-i', '--input',
-	help = 'Alignment file to mark ancestral sequence in, can be gzip compressed', 
-	required = True)
+	help='Alignment file to mark ancestral sequence in, can be gzip compressed', 
+	required=True)
 parser.add_argument('-o', '--output',
-	help = 'Name of output file, will be compressed if ending with .gz',
-	required = True)
+	help='Name of output file, will be compressed if ending with .gz',
+	required=True)
 parser.add_argument('-a', '--ancestor',
-	help = 'The name used to mark the ancestral sequence, eg. Pig_Cow',
-	required = True)
+	help='The name used to mark the ancestral sequence, eg. Pig_Cow',
+	required=True)
 parser.add_argument('-l', '--logfile',
-	help = 'Log file (default: mark_ancestor.log)',
-	default = "mark_ancestor.log")
+	help='Log file (default: mark_ancestor.log)',
+	default="mark_ancestor.log")
 parser.add_argument('--sp1-label',
-	help = 'alignment label/name of the species of interest e.g. sus_scrofa', 
-	required = True)
+	help='alignment label/name of the species of interest e.g. sus_scrofa', 
+	required=True)
 parser.add_argument('--sp1-ab',
-	help = 'abbrevation of species of interest as in the tree e.g. Sscr', 
-	required = True)
+	help='abbrevation of species of interest as in the tree e.g. Sscr', 
+	required=True)
 parser.add_argument('--sp2-ab',
-	help = 'abbrevation of species 2 as in the tree e.g. Btau',
-	required = True)
+	help='abbrevation of species 2 as in the tree e.g. Btau',
+	required=True)
 
 def hierarchy(found_clades, args):
 	"""
-	This function takes in a list of Bio.Phylo.BaseTree.Clade objects ,
+	This function takes in a list of Bio.Phylo.BaseTree.Clade objects,
 	which represent the terminal taxa of the investigated tree. The function
 	iterates over these taxa to identify if the given species is available and
 	returns the taxa which should be used for the identification of the last
@@ -87,78 +92,72 @@ def hierarchy(found_clades, args):
 		return [sp1, sp2]
 	return []
 
-# print('Marking ancestor in file: {}'.format(file))
-
 def main(args):
 	"""
 	Main function. Opens relevant file. Iterates through lines in maf file.
 	Iterate until I hit '# tree:' and read in the newick string and identify
-	if there is a common anecestor If yes, write out the ancestral
+	if there is a common ancestor. If yes, write out the ancestral
 	identifier and the information to which chromosome it was aligned,
 	to save the ancestral sequence according to the chromosome it belongs,
-	this will ease the aligning effort ,since it will only have to align to
+	this will ease the aligning effort, since it will only have to align to
 	the chromosome it belongs to and will not have to take all chromosomes
 	into account.
 	:param args: argparse parameters
 	:return: None
 	"""
 
-	alignment_file = gzip.open(args.input, "rt") \
-		if args.input.endswith('.gz') else open(args.input, "r")
+	try:
+		with gzip.open(args.input, "rt") if args.input.endswith('.gz') else open(args.input, "r") as alignment_file:
+			with gzip.open(args.output, "wt", compresslevel=1) if args.output.endswith('.gz') else open(args.output, "w") as outfile:
+				with open(args.logfile, 'w') as log_s:
+					log_s.write(f"Logfile from mark_ancestor.py\nScript arguments:\n{args}\n")
 
-	outfile = gzip.open(args.output, "wt", compresslevel = 1) \
-		if args.output.endswith('.gz') else open(args.output, "w")
+					ancestor_id = ''
+					i = 0
+					elem = None
+					ancestor_seq = None
 
-	log_s = open(args.logfile, 'w')
-	log_s.write(f"Logfile from mark_ancestor.py\nScript arguments:\n{args}\n")
+					for lines in alignment_file:
+						to_write = True
+						# Checking if tree contains information regarding ancestor
+						if '# tree:' in lines:
+							ancestor_seq = None
+							elem = None
+							i += 1
+							lines = lines.strip()
+							to_write = False
 
-	ancestor_id = ''
-	i = 0
-	elem = None
-	ancestor_seq = None
+							# Processing the tree to identify the ID of the last common ancestor
+							newick = lines.strip().split(' ')[-1]
+							tree = Phylo.read(StringIO(newick), "newick")
 
-	for lines in alignment_file:
-		to_write = True
-		# Checking if tree contains information regarding ancestor
-		if '# tree:' in lines:
-			ancestor_seq = None
-			elem = None
-			i += 1
-			lines = lines.strip() # added
-			to_write = False
+							clades = hierarchy(tree.get_terminals(), args)
 
-			# Processing the tree to idenfy the ID of the last common ancestor
-			newick = lines.strip().split(' ')[-1]
-			tree = Phylo.read(StringIO(newick), "newick")
+							if len(clades) == 0:
+								ancestor_id = 'not_available'
+								log_s.write(str(i) + '\t' + ancestor_id + '\t' + 'NA' + '\n')
+							else:
+								ancestor_id = '_'.join(tree.common_ancestor(clades).name.split('_')[1:4])
+								log_s.write(str(i) + '\t' + ancestor_id + '\t' + args.ancestor + '\n')
+						elif lines.startswith(f's {args.sp1_label}.'):
+							# Stores coords for ancestor
+							elem = lines.strip().split()[0:-1]
+						elif lines.startswith('s ancestral_sequences.' + ancestor_id):
+							to_write = False
+							ancestor_seq = lines.strip().split()[-1]
 
-			clades = hierarchy(tree.get_terminals(), args)
+						# Write renamed/annotated ancestral alignment if fields found.
+						if ancestor_seq and elem:
+							outfile.write('s ' + args.ancestor + '.' +	
+								elem[1].split('.')[1] + '\t' + '\t'.join(elem[2:]) + '\t' + ancestor_seq + '\n')
+							ancestor_seq, elem = None, None
 
-			if len(clades) == 0:
-				ancestor_id = 'not_available'
-				log_s.write(str(i) + '\t' + ancestor_id + '\t' + 'NA' + '\n')
-			else:
-				ancestor_id = '_'.join(tree.common_ancestor(clades).name.split('_')[1:4])
-				log_s.write(str(i) + '\t' + ancestor_id + '\t' + args.ancestor + '\n')
-		elif lines.startswith(f's {args.sp1_label}.'):
-			# Stores coords for ancestor
-			elem = lines.strip().split()[0:-1]
-		elif lines.startswith('s ancestral_sequences.' + ancestor_id):
-			to_write = False
-			ancestor_seq = lines.strip().split()[-1]
-
-		# Write renamed/annotated ancestral alignment if fields found.
-		if ancestor_seq and elem:
-			outfile.write('s ' + args.ancestor + '.' +	
-				elem[1].split('.')[1] + '\t' + '\t'.join(elem[2:]) + '\t' + ancestor_seq + '\n')
-			ancestor_seq, elem = None, None
-
-		# Write original line if not tree or ancestral sequence
-		if to_write:
-			outfile.write(lines)
-
-	outfile.close()
-	alignment_file.close()
-	log_s.close()
+						# Write original line if not tree or ancestral sequence
+						if to_write:
+							outfile.write(lines)
+	except Exception as e:
+		logging.error(f"Error processing files: {e}")
+		sys.exit(1)
 
 if __name__ == '__main__':
 	main(parser.parse_args())
