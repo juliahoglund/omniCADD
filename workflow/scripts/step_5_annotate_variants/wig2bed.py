8088a52d7afb0bb6ee09a529844a9d47fd10c2db
juliahoglund <63258050+juliahoglund@users.noreply.github.com>
2025-05-23 10:38:12 +0200

    convert to python from R and make sure header is not treated as value

--- workflow/scripts/step_5_annotate_variants/wig2bed.py
+++ workflow/scripts/step_5_annotate_variants/wig2bed.py
@@ -17,20 +17,23 @@ def main():
     for i, file in enumerate(files):
         chromosome = file.split('_')[0].replace('chr', '')
         part = pd.read_csv(os.path.join(args.directory, file), header=None, sep="\t")
+
+        # Skip rows that contain header
+        part = part[~part.iloc[:, 0].astype(str).str.startswith("fixedStep")].reset_index(drop=True)
+
         part['chromosome'] = chromosome
 
-        if i == 0:
-            part['start'] = range(len(part))
-        else:
-            part['start'] = range(last_row + 1, last_row + 1 + len(part))
-
+        # Update start positions to ensure continuity across files and make them 1-based
+        part['start'] = range(last_row + 1, last_row + 1 + len(part))
         part['end'] = part['start'] + 1
+
         part['score'] = part.iloc[:, 0]
         part = part[['chromosome', 'start', 'end', 'score']]
 
         output_file = os.path.join(args.directory, f"{file.split('.')[0]}.{args.type}.bed")
         part.to_csv(output_file, header=False, index=False, sep="\t")
 
+        # Update last_row to the last end position
         last_row = part['end'].iloc[-1]
 
 if __name__ == "__main__":
